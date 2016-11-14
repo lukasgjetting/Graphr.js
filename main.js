c884e64bf96f787f7a27455296d09a043fd0b654
Lukas Gjetting <lukasgjetting@gmail.com>
2016-11-14 10:21:27 +0100

    Enable dragging

--- main.js
+++ main.js
@@ -7,15 +7,22 @@ const width = canvas.clientWidth;
 
 // 100%
 let zoom = 100;
-let position = [-2, -2];
-
+let position = {
+	x: 0,
+	y: 0
+};
+let isDragging = false;
+let lastPosition = {
+	x: 0,
+	y: 0
+};
 const loop = setInterval(function(){
 	update();
 	render();
-}, 500);
+}, 1000/60);
 
 function update() {
-	// TODO
+
 }
 
 function render() {
@@ -23,9 +30,30 @@ function render() {
 
 	// Draw x and y-axis
 	context.beginPath();
-	context.moveTo(-position[0]*zoom, 0);
-	context.lineTo(-position[0]*zoom, height);
-	context.moveTo(0,-position[1]*zoom);
-	context.lineTo(width,-position[1]*zoom);
+	context.moveTo(-position.x*zoom, 0);
+	context.lineTo(-position.x*zoom, height);
+	context.moveTo(0, height-position.y*zoom);
+	context.lineTo(width, height-position.y*zoom);
 	context.stroke();
 }
+
+// Event listeners
+canvas.addEventListener("mousedown", function() {
+	isDragging = true;
+});
+canvas.addEventListener("mouseup", function() {
+	isDragging = false;
+});
+canvas.addEventListener("mousemove", function(event){ 
+	if(isDragging) {
+
+		position.x += (lastPosition.x - event.clientX)/100;
+		position.y += (lastPosition.y - event.clientY)/100;
+
+		console.log(position);
+	}
+
+	lastPosition.x = event.clientX;
+	lastPosition.y = event.clientY;
+
+});
